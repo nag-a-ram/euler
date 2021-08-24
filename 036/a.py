968229634ebe5a94c3880dd0bb1e905e1f83260f
Alee Azam <86848866+aleeazam@users.noreply.github.com>
2021-08-24 13:27:33 +0500

    write a.py

---
+++ 036/a.py
@@ -0,0 +1,18 @@
+"""
+Copyright 2021 Alee Azam
+--------- ---- ---- ----
+
+real    0m0.701s
+user    0m0.684s
+sys     0m0.017s
+"""
+
+total = 0
+
+for i in range(1, 1000001):
+    b10, b2 = str(i), str(bin(i))[2:]
+
+    if b10 == b10[::-1] and b2 == b2[::-1]:
+        total += i
+
+print(total)
